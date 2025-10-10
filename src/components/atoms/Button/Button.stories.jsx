10df8869ca0bd46ee6146a65c99d5c8c842803c8
Hampus von Horn <hampus.vh@gmail.com>
2025-10-10 12:48:59 +0200

    chore(storybook): update Button stories to include variant prop

--- src/components/atoms/Button/Button.stories.jsx
+++ src/components/atoms/Button/Button.stories.jsx
@@ -1,15 +1,22 @@
 import Button from "./Button";
-import { fn } from 'storybook/test';
+import { fn } from "storybook/test";
 
 export default {
   title: "Atoms/Button",
   component: Button,
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: ["primary", "secondary", "text"],
+    },
+  },
 };
 
 export const Default = {
   args: {
     children: "Default button",
     onClick: fn(),
+    variant: "primary",
   },
 };
 
@@ -18,10 +25,8 @@ export const Disabled = {
     children: "Disabled button",
     disabled: true,
     onClick: fn(),
+    variant: "primary",
   },
 };
 
-export const MissingArgs = {
-  args: {
-  },
-};
+export const MissingArgs = { args: {} };
