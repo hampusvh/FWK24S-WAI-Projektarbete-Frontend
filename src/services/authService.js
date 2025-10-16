3bd430b47eec414761a00cf1cb0a733a8eec492a
andreasLoetzsch <168843741+andreasLoetzsch@users.noreply.github.com>
2025-10-16 10:07:39 +0200

    feat: added recpatha

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,7 @@ export const useAuthService = () => {
   const { auth } = useApi();
 
   const register = (email, username, password, role, phoneNumber) => auth.post("/auth/register", { email, username, password, role, phoneNumber });
-  const login = (username, password) => auth.post("/auth/login", {username, password});
+  const login = (username, password, token) => auth.post("/auth/login", {username, password, token});
   const logout = () => auth.post("/auth/logout", {});
 
   return { register, login, logout };
