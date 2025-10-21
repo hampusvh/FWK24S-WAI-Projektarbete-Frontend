8dc8448c68e44d7f8aa2ff4fac0de60e605488d5
Hampus von Horn <hampus.vh@gmail.com>
2025-10-21 10:29:46 +0200

    feat: add basic Terms page and link from Register form

---
+++ src/pages/TermsPage/TermsPage.jsx
@@ -0,0 +1,11 @@
+import AgreementTerms from "../../components/organisms/AgreementTerms/AgreementTerms";
+
+const TermsPage = () => {
+  return (
+    <div>
+      <AgreementTerms />
+    </div>
+  );
+};
+
+export default TermsPage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/LoginPage/LoginPage";
 import Register from "../pages/RegisterPage/RegisterPage";
 import HomePage from "../pages/HomePage/HomePage";
+import TermsPage from "../pages/TermsPage/TermsPage";
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/terms" element={<TermsPage />} />
   </Routes>
 );
 
