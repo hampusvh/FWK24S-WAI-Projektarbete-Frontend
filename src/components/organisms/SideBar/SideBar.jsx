bb46b8820706c3b647ed1029558090e3a52ec817
Hampus von Horn <hampus.vh@gmail.com>
2025-10-23 14:05:44 +0200

    add LogoutButton to SideBar

--- src/components/organisms/SideBar/SideBar.jsx
+++ src/components/organisms/SideBar/SideBar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./SideBar.module.css";
 
+import LogoutButton from "../../molecules/LogoutButton/LogoutButton";
+
 const SideBar = () => {
-  return <div className={styles.sidebar_container}></div>;
+  return (
+    <div className={styles.sidebar_container}>
+      <LogoutButton />
+    </div>
+  );
 };
 
 export default SideBar;
