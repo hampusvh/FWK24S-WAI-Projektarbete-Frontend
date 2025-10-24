4555d38997d81c04585e896396619402978e8af8
Hampus von Horn <hampus.vh@gmail.com>
2025-10-24 09:21:41 +0200

    fix HomePage layout structure

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
+import React from "react";
 import SideBar from "../../components/organisms/SideBar/SideBar";
+import JournalSheet from "../../components/organisms/JournalSheet/JournalSheet";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div className="home-page">
+    <div className={styles.homePage}>
       <SideBar />
-      <main className="main-content"></main>
+      <main className={styles.mainContent}>
+        <JournalSheet />
+      </main>
     </div>
   );
 };
