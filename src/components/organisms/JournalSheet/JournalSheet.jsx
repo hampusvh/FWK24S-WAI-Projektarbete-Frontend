1590edc004da2b73fe9a49eedbd1a7c441e68ed9
Hampus von Horn <hampus.vh@gmail.com>
2025-10-24 09:22:15 +0200

    add basic  organism: JournalSheet

---
+++ src/components/organisms/JournalSheet/JournalSheet.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./JournalSheet.module.css";
+
+const JournalSheet = () => {
+  return <div className={styles.journalSheet}></div>;
+};
+
+export default JournalSheet;
---
+++ src/components/organisms/JournalSheet/JournalSheet.stories.jsx
@@ -0,0 +1,10 @@
+import JournalSheet from "./JournalSheet";
+
+export default {
+  title: "Organisms/JournalSheet",
+  component: JournalSheet,
+};
+
+export const Default = {
+  args: {},
+};
