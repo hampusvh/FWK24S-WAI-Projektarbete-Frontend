2f694c44b5e8ad64c163c5ca5ae6921ae3fef2d1
Hampus von Horn <hampus.vh@gmail.com>
2025-10-28 11:56:31 +0100

    add Edit Profile button to UserSettingsSection

--- src/components/organisms/UserSettingsSection/UserSettingsSection.jsx
+++ src/components/organisms/UserSettingsSection/UserSettingsSection.jsx
@@ -2,10 +2,11 @@ import Button from "../../atoms/Button/Button";
 import Card from "../../atoms/Card/Card";
 import styles from './UserSettingsSection.module.css';
 
-const UserSettingsSection = ({ onLogout, onDelete }) => {
+const UserSettingsSection = ({ onEdit, onLogout, onDelete }) => {
   return (
     <section>
       <Card className={styles.container}>
+        <Button onClick={onEdit}>Edit Profile</Button>
         <Button onClick={onLogout}>Logout</Button>
         <Button onClick={onDelete} variant="warning">Delete account</Button>
       </Card>
