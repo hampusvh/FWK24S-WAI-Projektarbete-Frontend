823b8481aa49729909cb9f15c726370efe7cf96e
Hampus von Horn <hampus.vh@gmail.com>
2025-10-28 11:34:48 +0100

    add MemoryRouter to SideBar.stories.jsx

--- src/components/organisms/SideBar/SideBar.stories.jsx
+++ src/components/organisms/SideBar/SideBar.stories.jsx
@@ -1,8 +1,16 @@
 import SideBar from "./SideBar";
+import { MemoryRouter } from "react-router-dom";
 
 export default {
   title: "Organisms/SideBar",
   component: SideBar,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
 export const Default = {
