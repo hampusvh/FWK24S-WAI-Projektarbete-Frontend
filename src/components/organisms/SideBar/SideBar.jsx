be2a77f5c08ac838a8194297851205f90996a9e6
Hampus von Horn <hampus.vh@gmail.com>
2025-10-28 11:34:26 +0100

    add journal placeholder button to SideBar

--- src/components/organisms/SideBar/SideBar.jsx
+++ src/components/organisms/SideBar/SideBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./SideBar.module.css";
-
+import Button from "../../atoms/Button/Button";
 import ProfileButton from "../../molecules/ProfileButton/ProfileButton";
 
 const SideBar = () => {
   return (
     <aside className={styles.sidebar}>
+      <Button>Journal</Button>
       <ProfileButton />
     </aside>
   );
