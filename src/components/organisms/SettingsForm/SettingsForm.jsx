8c1bca8df1350922675031f4fd7103f745115070
hampusvh <hampus.vh@gmail.com>
2025-11-04 11:40:06 +0100

    add organism: SettingsForm

---
+++ src/components/organisms/SettingsForm/SettingsForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Input from "../../atoms/Input/Input";
+import Button from "../../atoms/Button/Button";
+import { useAuth } from "../../../providers/AuthProvider";
+import styles from "./SettingsForm.module.css";
+
+const SettingsForm = ({handleSubmit}) => {
+  const { user } = useAuth();
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const [formData, setFormData] = useState({
+    email: user.email,
+    phoneNumber: user.phoneNumber,
+  });
+
+  return (
+    <div className={styles.settingsPage}>
+      <h1>Settings</h1>
+      <form className={styles.form} onSubmit={(e) => handleSubmit(e, formData)}>
+        <label>Change email</label>
+        <Input
+          name="email"
+          type="text"
+          placeholder="Edit email"
+          value={formData.email}
+          onChange={onChange}
+        />
+        <label>Change phone number</label>
+        <Input
+          name="phoneNumber"
+          type="text"
+          placeholder="Edit phone number"
+          value={formData.phoneNumber}
+          onChange={onChange}
+        />
+        <Button type="submit">Save</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SettingsForm;
---
+++ src/components/organisms/SettingsForm/SettingsForm.stories.jsx
@@ -0,0 +1,10 @@
+import SettingsForm from "./SettingsForm";
+
+export default {
+  title: "Organisms/SettingsForm",
+  component: SettingsForm,
+};
+
+export const Default = {
+  args: {}
+};
