028c8dcde06bb2abbc7c64be3d3e4cc597db4605
angelika-friis <angelikafriis@gmail.com>
2025-11-06 08:17:29 +0100

    add Linebreak

---
+++ src/components/atoms/LineBreak/LineBreak.jsx
@@ -0,0 +1,8 @@
+import styles from './LineBreak.module.css';
+
+const LineBreak = () => {
+  return (
+    <div className={styles.lineBreak}></div>
+  )
+}
+export default LineBreak
---
+++ src/components/atoms/LineBreak/LineBreak.stories.jsx
@@ -0,0 +1,8 @@
+import LineBreak from "./LineBreak";
+
+export default {
+  title: "Atoms/LineBreak",
+  component: LineBreak
+};
+
+export const Default = {};
