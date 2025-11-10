fffde17c4f9405a233f29191e9cac6c10a03a08a
Akke <sirpurpleness@gmail.com>
2025-11-10 12:00:06 +0100

    feat: improved security

--- src/components/atoms/Logo/Logo.jsx
+++ src/components/atoms/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 const Logo = () => {
-    return <img src="/images/mindjournal.png" alt="Logo" style={{ width: '150px', height: 'auto' }} />;
+    return <img src="/images/mindjournal.webp" alt="Logo" style={{ width: '150px', height: 'auto' }} />;
 }
 
 export default Logo;
